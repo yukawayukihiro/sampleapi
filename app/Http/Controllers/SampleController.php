0740d906662568b0dba93d1a29a68841f02ff709
yukawa <metalg9123@i.softbank.jp>
2023-04-17 23:07:36 +0900

    sampleapi

---
+++ app/Http/Controllers/SampleController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SampleController extends Controller
+{
+    public function apiHello () {
+        return 'Hello World';
+    }
+}
